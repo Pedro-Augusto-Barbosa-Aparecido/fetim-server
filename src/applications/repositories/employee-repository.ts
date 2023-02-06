647f6cbc3c402924f9f19d0039faa10a5cf950e2
Pedro Augusto Barbosa Aparecido <pedro007augustobarbosa@gmail.com>
2023-02-05 21:03:23 -0300

    delete employees and update prisma dependency

--- src/applications/repositories/employee-repository.ts
+++
@@ -1,5 +0,0 @@
-import { Employee } from "@applications/entities/employee";
-
-export abstract class EmployeeRepository {
-  abstract create(employee: Employee): Promise<void>;
-}
--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,16 +1,14 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
-import { EmployeeRepository } from "@applications/repositories/employee-repository";
-import { PrismaEmployeeRepositories } from "./prisma/repositories/prisma-employee-repository";
+
 import { SupportRepository } from "@applications/repositories/support-repository";
 import { PrismaSupportRepositories } from "./prisma/repositories/prisma-support-repository";
 
 @Module({
   providers: [
     PrismaService,
-    { provide: EmployeeRepository, useClass: PrismaEmployeeRepositories },
     { provide: SupportRepository, useClass: PrismaSupportRepositories },
   ],
-  exports: [EmployeeRepository, SupportRepository],
+  exports: [SupportRepository],
 })
 export class DatabaseModule {}
