ed3a8ea7ad2b0a2dcf4f98fbdec41da2beedc31b
Pedro Augusto Barbosa Aparecido <pedro007augustobarbosa@gmail.com>
2023-02-05 22:15:08 -0300

    configure simple email sender

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { DatabaseModule } from "@infra/database/database.module";
 import { HttpModule } from "@infra/http/http.module";
-import { Module } from "@nestjs/common";
+import { MailerModule } from "@nestjs-modules/mailer";
+import { BullModule } from "@nestjs/bull";
 
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: "localhost",
+        port: 32768,
+      },
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAIL_HOST,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
+        },
+      },
+    }),
+    HttpModule,
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/infra/http/controller/support.controller.ts
+++ src/infra/http/controller/support.controller.ts
@@ -1,11 +1,15 @@
 import { SupportCreate } from "@applications/useCases/SupportCreateUseCase";
+import { MailerService } from "@nestjs-modules/mailer/dist";
 import { Body, Controller, Post } from "@nestjs/common";
 import { CreateSupportBody } from "../dtos/support-create-body-dto";
 import { SupportViewModel } from "../view-models/support-view-models";
 
 @Controller("support")
 export class SupportController {
-  constructor(private supportCreate: SupportCreate) {}
+  constructor(
+    private supportCreate: SupportCreate,
+    private mailer: MailerService
+  ) {}
 
   @Post()
   async create(@Body() body: CreateSupportBody) {
@@ -14,6 +18,13 @@ export class SupportController {
     const { support } = await this.supportCreate.execute({
       message,
       requester,
+    });
+
+    await this.mailer.sendMail({
+      to: "pedr.augustobarbosa.aparecido@gmail.com",
+      from: "Fetim Stapp <pedro007augustobarbosa@gmail.com>",
+      subject: "Novo suporte requisitado",
+      text: `Òlá, tem um novo suporte para resolver!\n\n\n${message}\n\n\n\nRequester: ${requester}`,
     });
 
     return {
