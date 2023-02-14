18dbca75acf27c651a43aaa98214684b1ffb4b10
Pedro Augusto Barbosa Aparecido <pedro007augustobarbosa@gmail.com>
2023-02-13 22:32:13 -0300

    exclude unused testings

--- test/repositories/in-memory-employee-repository.ts
+++
@@ -1,6 +0,0 @@
-import { Employee } from "src/applications/entities/employee";
-import { EmployeeRepository } from "src/applications/repositories/employee-repository";
-
-export class InMemoryDatabaseEmployee implements EmployeeRepository {
-  public employees: Employee[] = [];
-}
